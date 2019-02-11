c463d60e4e41c793abad1472a7ed603c3c939556
Matthew Stumphy <gurustump@gurustump.com>
2019-02-11 01:08:00 -0700

    adding dark theme

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!doctype html>
-
+<?php $colorTheme = get_option('bachelor_main_options')['display_theme']; ?>
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="b-o-p no-js"><!--<![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="b-o-p no-js theme-<?php echo $colorTheme; ?>"><!--<![endif]-->
 
 	<head>
 		<meta charset="utf-8">
