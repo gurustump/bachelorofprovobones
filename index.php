d24be0835d8838759565adf5ff32944f751c8f51
Matthew Stumphy <gurustump@gurustump.com>
2019-03-03 18:57:43 -0700

    adding posts index page layout and styles

--- index.php
+++ index.php
@@ -1,75 +1,74 @@
 <?php get_header(); ?>
 
 			<div id="content">
-
 				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-                                                                        <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
-
-								</header>
-
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
+						<?php $postPageModules = get_posts(array( 
+							'numberposts' => -1,
+							'post_type' => 'modules',
+							'meta_query' => array(
+								array(
+									'key' => '_bachelor_module_location',
+									'value' => 'post-index',
+									'compare' => 'LIKE',
+								),
+							),
+							'order' => 'ASC',
+							'orderby' => 'meta_value',
+							'meta_key' => '_bachelor_module_priority',
+						)); 
+						$page_title = get_the_title( get_option('page_for_posts', true) );
+						?>
+						<main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+							<header class="page-header">
+								<h1 class="page-title" itemprop="headline"><?php echo $page_title; ?></h1>
+							</header> <?php // end article header ?>
+							<div<?php echo $postPageModules ? ' class="has-content-secondary"':''; ?>>
+								<div class="content-primary">
+									<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+									<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+										<header class="article-header">
+											<h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+											<p class="byline vcard">
+												<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+											</p>
+										</header>
+										<section class="entry-content cf">
+											<?php the_excerpt(); ?>
+										</section>
+									</article>
+									<?php endwhile; ?>
+											<?php bones_page_navi(); ?>
+									<?php else : ?>
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+												<h2><?php _e( 'Coming Soon', 'bonestheme' ); ?></h2>
 										</header>
 											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+												<p><?php _e( 'Looks like we have yet to post any content here. Try back later.', 'bonestheme' ); ?></p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+												<!--<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>-->
 										</footer>
 									</article>
-
-							<?php endif; ?>
-
-
+									<?php endif; ?>
+								</div>
+								<?php if ($postPageModules) { ?>
+								<div class="content-secondary">
+									<?php if ($postPageModules) { ?>
+									<div class="modules">
+										<?php foreach($postPageModules as $module) { ?>
+										<div class="module">
+											<h2 class="module-title"><?php echo $module->post_title; ?></h2>
+											<div class="module-content"><?php echo wpautop(do_shortcode($module->post_content)); ?></div>
+										</div>
+										<?php } ?>
+										</div>
+									<?php } ?>
+								</div>
+								<?php } ?>
+							</div>
 						</main>
-
-					<?php get_sidebar(); ?>
-
 				</div>
-
 			</div>
 
 
